cf011c93dc10c8e6600e51ab408730c56d61b7f7
LyszczarzKrzysztof <krzysiek.lyszczarz@o2.pl>
2020-03-23 06:34:14 +0100

    slf4j-classpath-multiple-bindings.

---
+++ src/test/java/com/example/rectangle/RectangleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rectangle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RectangleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
