c2dff8eb6ea970cab9d86fc340bf4269c57396a3
LyszczarzKrzysztof <krzysiek.lyszczarz@o2.pl>
2020-03-26 15:26:32 +0100

    DB enabled.

---
+++ src/test/java/com/example/rectangleapp/RectangleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rectangleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RectangleAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
