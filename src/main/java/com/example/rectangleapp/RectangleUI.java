e8f358f2add24fd355f11e1dc1f7ee793e8f2bd8
LyszczarzKrzysztof <krzysiek.lyszczarz@o2.pl>
2020-03-26 15:12:00 +0100

    DB enabled.

--- src/main/java/com/example/rectangleapp/RectangleUI.java
+++ src/main/java/com/example/rectangleapp/RectangleUI.java
@@ -9,18 +9,33 @@ import com.vaadin.flow.router.Route;
 @Route
 public class RectangleUI extends VerticalLayout {
 
+    private RectangleRepository rectangleRepository;
+
     private TextField textFieldHeight;
     private TextField textFieldWidth;
     private Button button;
 
-    public RectangleUI() {
+
+    public RectangleUI(RectangleRepository rectangleRepository) {
+
+        this.rectangleRepository = rectangleRepository;
+
         textFieldHeight = new TextField("Podaj wysokość: ");
         textFieldWidth = new TextField("Podaj szerokość: ");
         button = new Button("Dodaj!");
 
+        button.addClickListener(clickEvent-> addRectangle());
+
         add(textFieldHeight);
         add(textFieldWidth);
         add(button);
+    }
+
+    public void addRectangle(){
+        Rectangle rectangle = new Rectangle();
+        rectangle.setHeight(Integer.parseInt(textFieldHeight.getValue()));
+        rectangle.setWidth(Integer.parseInt(textFieldWidth.getValue()));
+        rectangleRepository.save(rectangle);
     }
 
 }
